d1d5151c55624caebc4b87c3353c97801dae219f
chi karl <kchi8573@gmail.com>
2021-06-26 06:18:18 +0100

    Try working with task 2

--- task2.py
+++ task2.py
@@ -1,2 +1,15 @@
-#Coming soon 
-# the question has a problem please check and correct
+i = True
+
+def isprime(num):
+    for i in range(2,num):
+        if not(num%i):
+            return False
+        return True
+
+while i == True:
+    num = int(input('input a num or type 12 to stop: '))
+    if num == 12:
+        break
+    if isprime(num):
+            print(num)
+    
